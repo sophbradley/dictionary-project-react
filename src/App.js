836f8564e8cee49e154b5adbacc837f9acbfa175
sophbradley <111533096+sophbradley@users.noreply.github.com>
2023-02-20 19:13:34 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+      <button type="button" class="btn btn-info">
+        Test Button
+      </button>
+    </div>
+  );
+}
