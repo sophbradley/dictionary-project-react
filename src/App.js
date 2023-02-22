bb8a4ba947661d421503204fca52b1b1d4861309
sophbradley <111533096+sophbradley@users.noreply.github.com>
2023-02-22 19:12:41 +0000

    Added footer with links to GitHub and portfolio

--- src/App.js
+++ src/App.js
@@ -9,6 +9,25 @@ export default function App() {
           <h1>Dictionary</h1>
         </header>
         <Dictionary />
+        <footer className="mt-5 mb-5">
+          <a
+            href="https://github.com/sophbradley/dictionary-project-react"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Check it out on GitHub"
+          >
+            Open source code
+          </a>{" "}
+          built by{" "}
+          <a
+            href="https://www.sophiebradley.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            title="Check out my portfolio here"
+          >
+            Sophie Bradley
+          </a>
+        </footer>
       </div>
     </div>
   );
